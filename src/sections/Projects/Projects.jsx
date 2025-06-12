118a622b28f4df44d7e940d91679ec8992afa726
RiveroDev <samuelenriquerivero@gmail.com>
2025-06-12 14:29:03 -0500

    Finishing projects structure

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import styles from "./ProjectsStyles.module.css";
 import cvGenerator from "../../assets/cv-generator.png";
+import serena from "../../assets/serena.png";
+import crm from "../../assets/crm.png";
+import ProjectCard from "../common/ProjectCard";
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
-    <h1 className="sectionTitle">Projects</h1>
-    <div className={styles.projectsContainer}>
-        <img className="hover" src={cvGenerator} alt="CV Generator" />
-        <h3></h3>
-    </div>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard src={cvGenerator} alt="CV Generator" />
+        <ProjectCard src={serena} alt="Serena" />
+        <ProjectCard src={crm} alt="CRM" />
+      </div>
     </section>
   );
 }
